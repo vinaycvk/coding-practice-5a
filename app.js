91d042128fb418a0fdcfe0d1045387d415c3abd1
vinaycvk <ch.vinay518@gmail.com>
2023-02-08 18:36:38 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "moviesData.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+const convertSnakeToCamel = (dbObject) => {
+  return {
+    movieId: dbObject.movie_id,
+    directorId: dbObject.director_id,
+    movieName: dbObject.movie_name,
+    leadActor: dbObject.lead_actor,
+  };
+};
+
+app.get("/movies/", async (request, response) => {
+  const getMovieQuery = `
+    SELECT
+      movie_name
+    FROM
+      movie
+    ORDER BY
+      movie_id;`;
+  const movieArray = await db.all(getMovieQuery);
+  response.send(movieArray.map((eachMovie) => convertSnakeToCamel(eachMovie)));
+});
+
+app.post("/movies/", (request, response) => {
+  const movieDetails = request.body;
+  console.log(movieDetails);
+});
